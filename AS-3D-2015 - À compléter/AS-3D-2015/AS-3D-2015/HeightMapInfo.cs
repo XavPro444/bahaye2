192ae5362cf7408eb977fd6fc1c546a0c33b720f
olidugal <201451968@P106-16.clg.priv>
2016-02-22 15:04:20 -0500

    update

---
+++ AS-3D-2015 - À compléter/AS-3D-2015/AS-3D-2015/HeightMapInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework;
+
+namespace AtelierXNA
+{
+    class HeightMapInfo
+    {
+        private float tailleTerrain { get; set; }
+        private float[,] hauteur { get; set; }
+        private Vector3[,] normales { get; set; }
+
+
+
+
+
+    }
+}
